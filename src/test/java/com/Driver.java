2316676cce933d64b87b715a40250bf3c033e450
IBI AWESU <IBI AWESU>
2016-03-29 17:30:28 +0100

    Initial Commit

---
+++ src/test/java/com/Driver.java
@@ -0,0 +1,40 @@
+package com;
+
+
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Ibi on 04/02/2016.
+ */
+public class Driver {
+
+
+    public WebDriver driver;
+
+    @Before
+    public void setUp(){
+
+       driver= new FirefoxDriver();
+        System.out.println("Lunching browser");
+    }
+
+    @Test
+    public void navigate(){
+
+        driver.get("https://www.google.co.uk/?gfe_rd=cr&ei=9qT6Vt2dMYbW8AeawImQDQ&gws_rd=ssl");
+        System.out.println("open application");
+    }
+    @After
+    public void tearDown(){
+
+        driver.close();
+        System.out.println("quiting browser");
+    }
+
+}
